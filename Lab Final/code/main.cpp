70ae4a83474f2926c3f09428483132dc8fefa68c
Sadia Anha Tonni <sadiaanha766@gmail.com>
2021-09-25 03:13:42 +0600

    Add files via upload

--- Lab Final/code/main.cpp
+++ Lab Final/code/main.cpp
@@ -1,87 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
-FILE *fp , *fp2;
+using namespace std;
 
-void check_comment(char a)
+int main()
 {
-    char x;
-
-    if( a == '/')   //checking if the character starts with '/', it will be a comment
-    {
-        if((x=fgetc(fp))=='*')
-         check_block_comment();
-
-        else if( x == '/')   // else if the next character '/', it is the beginning of single line comment
-        {
-          check_single_comment();
-
-        }
-        else
-        {
-            // when both the cases fail then it is not a comment
-            fputc(a,fp2);
-            fputc(x,fp2);
-        }
-    }
-
-    // when all the conditions are false, add the character as it is in the new file.
-    else
-        fputc(a,fp2);
-}
-
-
-// function for block comments
-void check_block_comment()
-{
-
- char x,y;
-
-    while((x=fgetc(fp))!=EOF)   // the block comment has started
-    {
-
-        if(x=='*')
-        {
-            y=fgetc(fp);  // check if it ends
-
-            if(y=='/')
-                return;
-        }
-   }
-
-}
-
-
-// function for single line comments
-void check_single_comment()
-{
- char x,y;
-
-    while((x=fgetc(fp))!=EOF)
-    {
-
-        if(x=='\n')
-            return;  // if the comment ends return from the function
-
-    }
-
-}
-
-
-int main(void)
-{
-    char c;
-
-    fp = fopen ("testfile.txt","r") ;   // first file in read mode
-    fp2 = fopen ("solved.txt","w") ;    // second file in write mode
-
-    while(
-          (c=fgetc(fp))!=EOF)
-        check_comment(c);   // checking for the beginning of a comment
-
-     //  closing both files
-    fclose(fp);
-    fclose(fp2);
-
+    cout << "Hello world!" << endl;
     return 0;
 }
